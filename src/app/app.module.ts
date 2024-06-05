c42ece4ef9574cc17e1128a16c65fbaad98e2a3f
felipetop <felipejorgetop@gmail.com>
2024-06-04 23:30:26 -0300

    feat: add register module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { RegisterModule } from './register/register.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RegisterModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
